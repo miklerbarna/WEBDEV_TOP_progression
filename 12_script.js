1e27a2b3448aa00071b16fc2addc5ae877c9ffdc
Wmiklerbarna <miklerbarna2@gmail.com>
2022-07-02 11:21:13 +0200

    Started Javascript basics, Created 12_javascript-basics.html and its script

---
+++ 12_script.js
@@ -0,0 +1,30 @@
+console.log('Hello');
+
+
+//alert('Hi, this is an alert');
+
+
+let message;
+message = 'Hello World';
+console.log(message);
+
+
+let message2 = 'Hello again';
+console.log(message2);
+
+//You can also do this
+
+let name = 'John',
+    age = 25,
+    fav_color = 'blue';
+
+console.log('Name: ' + name + ' Age: ' + age + '\nFavourite Color: ' + fav_color);
+
+let a = 10,
+    b = 5;
+console.log(a + b);
+
+
+let c = a;
+
+console.log(a + c);
