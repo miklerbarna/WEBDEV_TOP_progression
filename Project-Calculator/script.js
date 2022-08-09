82cc09e26f90e120759c0958706484a4faa75741
Wmiklerbarna <miklerbarna2@gmail.com>
2022-08-09 18:56:19 +0200

    Finished Project-Calculator

--- Project-Calculator/script.js
+++ Project-Calculator/script.js
@@ -1,6 +1,8 @@
 const buttons = document.querySelectorAll('button');
 const display = document.querySelector('#display');
-
+let input = '';
+let wasOperator = false;
+const operators = '+-*/';
 
 
 
@@ -14,7 +16,7 @@ function handleClick(e)
             handleEvaluate();
             break;
         default:
-            addToDisplay(e.target.dataset.contains);
+            handleButtonPress(e.target.dataset.contains);
             break;
     }
 }
@@ -22,17 +24,63 @@ function handleClick(e)
 function handleClear()
 {
     display.textContent = '';
+    input = '';
 }
 
 function handleEvaluate()
 {
-    console.log('more work here');
+    let num1 = ''
+    let i = 0;
+    while(!operators.includes(input[i]))
+    {
+        num1 += input[i];
+        i++;
+    }
+    
+    let op = input[i];
+    i++
+    let num2 = '';
+    while (i < input.length)
+    {
+        num2 += input[i];
+        i++;
+    }
+    console.log(num1);
+    console.log(op);
+    console.log(num2);
+
+    switch(op){
+        case '+':
+            input = parseInt(num1) + parseInt(num2);
+            display.textContent = `${parseInt(num1) + parseInt(num2)}`;
+            break;
+        case '-':
+            input = parseInt(num1) - parseInt(num2);
+            display.textContent = `${parseInt(num1) - parseInt(num2)}`;
+            break;
+        case '*':
+            input = parseInt(num1) * parseInt(num2);
+            display.textContent = `${parseInt(num1) * parseInt(num2)}`;
+            break;
+        case '/':
+            input = parseInt(num1) / parseInt(num2);
+            display.textContent = `${parseInt(num1) / parseInt(num2)}`;
+            break;
+    }
+    
 }
 
 
-function addToDisplay(char)
+function handleButtonPress(char)
 {
+    if (wasOperator && operators.includes(char))
+    {
+        console.log('operator after operator');
+        return;
+    }
     display.textContent += char;
+    input += char;
+    wasOperator = operators.includes(char);
 }
 
 
