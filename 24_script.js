fbbfb49f790854afd8bb28c8ff2f7dd09464bd1f
Wmiklerbarna <miklerbarna2@gmail.com>
2022-08-22 10:40:55 +0200

    Finished forms

--- 24_script.js
+++ 24_script.js
@@ -1,10 +1,17 @@
 const username = document.querySelector("#username");
 const submit = document.querySelector('.forJS');
 const display = document.querySelector('.display');
+const form = document.querySelector('form');
+const toggler = document.querySelector('form [type="button"]');
 
 
 
 
 submit.addEventListener('click', () => {
     display.textContent = `Hello, ${username.value}!`;
+})
+
+
+toggler.addEventListener('click', () => {
+    (getComputedStyle(form).backgroundColor === "rgb(0, 255, 255)") ? form.style.backgroundColor = "rgb(255, 0, 0)" : form.style.backgroundColor = "rgb(0, 255, 255)";
 })
